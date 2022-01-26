aa7e8bc9c06b05275cf76ed4f9e51d3a291db781
judith lourde <judith.lourde@gmail.com>
2022-01-27 00:20:22 +0100

    Intial commit

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import Home from './views/Home.vue';
+import Portfolio from './views/Portfolio.vue';
+import About from './views/About.vue';
+
+export default [
+	{ name: 'home', path: '/', component: Home },
+	{ name: 'portfolio', path: '/:portfolio', component: Portfolio },
+	{ name: 'about', 		path: '/about-me', 		component: About },
+];
+
