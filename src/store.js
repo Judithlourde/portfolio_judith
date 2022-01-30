6828a51dea316db4491dd56a88984313dd34a579
judith lourde <judith.lourde@gmail.com>
2022-01-30 23:42:46 +0100

    Added data in store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,36 @@
 export default {
 	state() {
-		return {};
+		return {
+			navigation: [
+				{ title: `Projects` },
+				{ title: `Capability` },
+				{ title: `Contact` },
+			],
+
+			about: [
+				{
+					intro: `Hi there, I'am Judith 👋`,
+					lead: `Front End Developer`,
+					aboutText: [
+						`I am an enthusiastic and detail-oriented Frontend software developer seeking a position with a company to use my skills in coding to provide solutions that meet customer needs through collaboration with cross-functional teams.`,
+					],
+					// author: '',
+					preview: {
+						image: '/images/judith_image.png',
+						caption: `Judith_image`
+					},
+				},
+			]
+		};
 	},
-};
+
+	getters: {
+		getNavigationPages(state) {
+			return state.navigation;
+		},
+
+		getAbout(state) {
+			return state.about;
+		}
+	}
+}
