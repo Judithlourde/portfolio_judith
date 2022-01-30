74aae47450a896b2151f20aa9dfcc51db5652908
judith lourde <judith.lourde@gmail.com>
2022-01-30 23:45:16 +0100

    Added mobile version of header

---
+++ src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+    <main class="about" v-for="page in aboutPage" :key="page.id">
+        <div class="about__container">
+            <div class="about__container-text">
+                <p>{{ page.intro }}</p>
+
+                <h2>{{ page.lead }}</h2>
+
+                <p>{{ page.aboutText[0] }}</p>
+            </div>
+
+            <div class="about__container-image">
+                <img src="/images/judith_image.png" alt="judith-image">
+            </div>
+        </div>
+        
+
+    </main> 
+</template>
+
+<script>
+    export default {
+        data() {
+            
+        },
+
+        computed: {
+            aboutPage() {
+                return this.$store.getters.getAbout;
+            }
+        }
+    }
+</script>
+
+<style>
+    .about {
+        width: 100%;
+        position: relative;
+        top: 120px;
+        display: grid;
+        grid-template-columns: repeat(12, 1fr);
+    }
+
+    .about__container {
+        grid-column: 2/ span 10 ;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        gap: var(--gap-big);
+    }
+
+    .about__container-text h2 {
+        padding: var(--padding-medium) 0;
+    }
+
+    .about__container-text p {
+        line-height: 1.5;
+    }
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,18 @@
 <template>
-	
+	<!-- <Header /> -->
+    <Home />
 </template>
+
+<script>
+import Header from '../components/Header.vue'
+import Home from '../components/Home.vue'
+
+ export default {
+        components: {
+            Header,
+            Home,
+        }
+        
+    }
+</script>
+
