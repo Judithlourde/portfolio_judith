0f13240c306c411471eea71e7ba81477ce629fef
judith lourde <judith.lourde@gmail.com>
2022-01-31 12:40:51 +0100

    Added buttons not linked yet

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,14 +7,26 @@
                 <h2>{{ page.lead }}</h2>
 
                 <p>{{ page.aboutText[0] }}</p>
+
+                <div class="about__container-buttons">
+                    <button>
+                        <a class="member" href="">View my work</a>
+                    </button>
+                    
+                    <button>
+                        <a href="">More about me</a>
+                    </button>
+                </div> 
             </div>
 
             <div class="about__container-image">
                 <img src="/images/judith_image.png" alt="judith-image">
             </div>
-        </div>
+        </div>  
+
         
 
+         
     </main> 
 </template>
 
@@ -39,6 +51,7 @@
         top: 120px;
         display: grid;
         grid-template-columns: repeat(12, 1fr);
+        font-size: 20px;
     }
 
     .about__container {
@@ -57,5 +70,44 @@
         line-height: 1.5;
     }
 
+    .about__container-buttons {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        
+    }
 
+    .about__container-buttons button {
+        margin-top: 30px;
+    }
+
+    .about__container-buttons button a {
+        padding-right: 40px;
+        text-decoration: none;
+        color: var(--secondary);
+        background: linear-gradient(#350348, #ae00f9cf);
+        padding: 10px 15px;
+        border-radius: 15px;
+        margin-right: 20px;
+    }
+
+    /* Medium screen devices (968px and above) */
+    @media screen and (min-width: 768px) {
+        .about__container {
+            display: grid;
+            grid-template-columns: repeat(12, 1fr);
+            position: relative;
+            top: 100px;
+            text-align: left;
+        }
+
+        .about__container-text {
+            grid-column: 2/ span 5;
+        }
+
+        .about__container-image {
+            grid-column: 8 / span 4;
+        }
+    }
 </style>
