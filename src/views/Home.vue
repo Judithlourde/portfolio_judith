c672e9c4652e48b57d9ba2cef13e8646e8890050
judith lourde <judith.lourde@gmail.com>
2022-02-05 22:41:49 +0100

    Fix Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
-	<!-- <Header /> -->
-    <Home />
+    <Header />
+    <About />
+    <Projects />
 </template>
 
 <script>
 import Header from '../components/Header.vue'
-import Home from '../components/Home.vue'
+import About from '../components/About.vue'
+import Projects from '../components/Projects.vue'
 
  export default {
         components: {
             Header,
-            Home,
+            About,
+            Projects,
         }
         
     }
